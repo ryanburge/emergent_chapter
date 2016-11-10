2daca78b1b05fd540e53c1be1cd5f0c239bfedec
Ryan Burge <ryanburge@gmail.com>
2016-11-10 15:36:51 -0600

    Adding some index syntax. Figured out a better way to histogram.

--- overlap_histogram.R
+++ overlap_histogram.R
@@ -1,10 +1,38 @@
 df <- read_dta("clergy.dta")
 df$relcon <- df$q68_1 + df$q68_2 + df$q68_3 + df$q68_4 + df$q68_5 + df$q68_6
+df$relcon <- df$relcon/30
+df$relcon <- round(df$relcon, 2)
 ggplot(df, aes(relcon)) +geom_bar()
 install.packages("psy")
 cronbach(df[,331:336])
 
+
+
+
+
+
+df$dem <- df$dem1 + df$dem2 + df$dem3 + df$dem4
+df$dem <- df$dem/20
+
+df$delib1 = 5- df$q47_1
+df$delib2 = 5- df$q47_2
+df$delib3 = 5- df$q47_3
+df$delib4 = 5- df$q47_4
+df$delib5 = 5- df$q47_5
+df$delib <- df$delib1 + df$delib2 + df$delib3 + df$delib4 + df$delib5
+df$delib <- df$delib/max(df$delib)
+
+
 emergent <- filter(df, q25 == 1 | q25==2)
+n_emergent <- filter(df, q25 ==3 | q25 ==4 | q25==5)
+
+
+emergent$label <- c("Emergent")
+n_emergent$label <- c("Non Emergent")
+df.plot <- rbind(n_emergent, emergent)
+
+
+
 
 ggplot(emergent, aes(relcon)) +geom_bar(aes(y = (..count..)/sum(..count..))) + scale_y_continuous(labels=percent)
 
@@ -12,7 +40,8 @@ ggplot(emergent, aes(relcon)) +geom_bar(aes(y = (..count..)/sum(..count..))) + s
 ggplot(df, aes(relcon)) + geom_bar(aes(y = (..count..)/sum(..count..)), data = emergent, fill = "white", color = "gray", alpha = 0.6) +  geom_bar(aes(y = (..count..)/sum(..count..)), data = n_emergent, fill = "gray",  alpha = 0.6) + scale_y_continuous(labels=percent) + ylab("Percent of Sample") + xlab("Religious Conservatism Scale") + theme(text=element_text(size=16, family="Roboto")) 
 
 
-ggplot(df.plot,aes(x=relcon/30, y = (..count..)/sum(..count..)))+ 
-  geom_histogram(data=subset(df.plot,label=='Emergent'),aes(fill=label),alpha=0.6)+
-  geom_histogram(data=subset(df.plot,label=='Entire Sample'),aes(fill=label),alpha=0.6)+
-  scale_fill_manual(name="label", values=c("black","gray"),labels=c("Non-Emergent","Emergent")) +  scale_y_continuous(labels=percent) + ylab("Percent of Sample") + xlab("Religious Conservatism Scale") + theme(text=element_text(size=16, family="Roboto")) +    theme(legend.position="bottom") + theme(legend.title = element_blank())
+ggplot(data=subset(df.plot, !is.na(relcon)), aes(x=factor(relcon), fill=label)) +
+  geom_bar(aes(y = (..count..)/sum(..count..)), position="dodge") + theme(axis.text.x = element_text(angle = 90)) +
+  scale_y_continuous(labels=percent) + ylab("Percent of Sample") + xlab("Religious Conservatism Scale") +
+  theme(text=element_text(size=16, family="Roboto")) +    theme(legend.position="bottom") + 
+  theme(legend.title = element_blank())
